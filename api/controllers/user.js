db3aa3d42e2b2446436d5f0ef281780d1c0f215b
MIKE <MiguelMartinezDW@outlook.com>
2018-12-13 14:22:33 -0600

    Se arreglo lo de duplicados

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -23,34 +23,44 @@ function pruebas(req, res) {
 /* Funcion de registro */
 function saveUser(req, res) {
     var params = req.body;
-    var user = new User(); /* Creamos un nuevo objeto de tipo usuario */
 
     /* En caso de que lo que se esta enviando es verdadero y copleto */
     if (params.name && params.surname && params.nick && params.email && params.password) {
-        /* Setea los valores de la variable user */
-        user.name = params.name;
-        user.surname = params.surname;
-        user.nick = params.nick;
-        user.email = params.email;
-        user.role = 'ROLE_USER';
-        user.image = null;
+        /* Compribamos que no exista un usuario ya registrado con el mismo correo */
+        User.findOne({ email: params.email }, (err, user) => {
+            if (err) return res.status(500).send({ message: 'Error en la petición' });
 
-        bcrypt.hash(params.password, null, null, (err, hash) => {
-            user.password = hash; /* Seteamos la variable de password con el nuevo hash generado con bcrypt */
+            if (!user) {
+                var user = new User(); /* Creamos un nuevo objeto de tipo usuario */
 
-            /* Se guarda el usuario en la base de datos */
-            user.save((err, userStored) => { /* Se ejecuta esta funcion */
-                /* Se ejecuta en caso de que marque un error */
-                if (err) return res.status(500).send({ message: 'Error al guardar el usuario' });
-                /* Se ejecuta en caso de que todo sea exitoso */
-                if (userStored) {
-                    res.status(200).send({ user: userStored }); /* Se debuelve un objeto con el objeto de usuario */
-                } else {
-                    /* en caos de que el userStored este mal */
-                    res.status(404).send({ message: 'No se ha registrado el usuario' });
-                }
-            })
-        }); /* Encriptamos la contrasena */
+                /* Setea los valores de la variable user */
+                user.name = params.name;
+                user.surname = params.surname;
+                user.nick = params.nick;
+                user.email = params.email;
+                user.role = 'ROLE_USER';
+                user.image = null;
+
+                bcrypt.hash(params.password, null, null, (err, hash) => {
+                    user.password = hash; /* Seteamos la variable de password con el nuevo hash generado con bcrypt */
+
+                    /* Se guarda el usuario en la base de datos */
+                    user.save((err, userStored) => { /* Se ejecuta esta funcion */
+                        /* Se ejecuta en caso de que marque un error */
+                        if (err) return res.status(500).send({ message: 'Error al guardar el usuario' });
+                        /* Se ejecuta en caso de que todo sea exitoso */
+                        if (userStored) {
+                            res.status(200).send({ user: userStored }); /* Se debuelve un objeto con el objeto de usuario */
+                        } else {
+                            /* en caos de que el userStored este mal */
+                            res.status(404).send({ message: 'No se ha registrado el usuario' });
+                        }
+                    })
+                }); /* Encriptamos la contrasena */
+            } else {
+                return res.status(200).send('El correo de usuario ya existe');
+            }
+        });
 
     } else { /* en caso de que sean algunos valores falsos */
         /* Retorna un mensaje por consola, definiendo el error */
