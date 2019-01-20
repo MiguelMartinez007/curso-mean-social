5b9b7dfe807f156459d6f63e52670fe194a3c098
MIKE <MiguelMartinezDW@outlook.com>
2019-01-20 07:41:18 -0600

    Se creo el metodo para guardar publicaciones nuevas

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -17,6 +17,30 @@ function probando(req, res){
     });
 }
 
+// Guarda nuevas publicaciones
+function savePublication(req, res){
+    // Recojemos los parametros que nos llegan desde el body
+    var params = req.body;
+
+    // En caso de que no nos llegue ningun texto de publicacion
+    if(!params.text) return res.status(200).send({message:'Debes de enviar un texto'});
+
+    // Se crea una nueva instancia del objeto publicacion
+    var publication = new Publication();
+    publication.text = params.text;
+    publication.file = null;
+    publication.user = req.user.sub;
+    publication.created_at = moment().unix();
+
+    publication.save((err, publicationStored) => {
+        if(err) return res.status(500).send({message:'Error al guardar la publicación'});
+        if(!publicationStored) res.status(404).send({message:'La publicación no ha sido guardada'});
+        res.status(200).send({publication: publicationStored});
+    })
+}
+
 module.exports = {
-    probando
+    probando,
+    // Guardar una publicacion nueva
+    savePublication
 }
