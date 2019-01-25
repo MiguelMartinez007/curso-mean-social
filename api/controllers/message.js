1c747c4dfdc6c9f1dc41f7ebcb1c8244159b1682
MIKE <MiguelMartinezDW@outlook.com>
2019-01-25 00:05:05 -0600

    Se creo la funcion para guardar los mensajes entre usuarios

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -11,7 +11,30 @@ function probando(req, res) {
     res.status(200).send({message: 'Hola que tal desde los mensajes pribados'});
 }
 
+// Envia los mensajes entre los usuarios
+function saveMessage(req, res) {
+    var params = req.body;
+    
+    // Ver si existen estas propiedades
+    if (!params.text && !params.receiver) return res.status(200).send({message: 'Envia los datos necesarios'});
+
+    var message = new Message();
+    message.emitter = req.user.sub;
+    message.receiver = params.receiver;
+    message.text = params.text;
+    message.create_at = moment().unix();
+
+    message.save((err, messageStored) => {
+        if (err) return res.status(500).send({message: 'Error en la petición'});
+        if (!messageStored) return res.status(500).send({message: 'Error al eviar el mensaje'});
+
+        return res.status(200).send({message: messageStored});
+    });
+}
+
 module.exports = {
     // Metodo de prueba
-    probando
+    probando,
+    // Metodo para enviar mensaje entre los usuarios
+    saveMessage
 }
--- api/routes/message.js
+++ api/routes/message.js
@@ -6,5 +6,6 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.get('/probando-md', md_auth.ensureAuth, messageController.probando);
+api.post('/message', md_auth.ensureAuth, messageController.saveMessage);
 
 module.exports = api;
