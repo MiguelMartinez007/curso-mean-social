61312d0ed4ee775d353d7ff3125d13f4e296ad3f
MIKE <MiguelMartinezDW@outlook.com>
2019-01-25 00:24:50 -0600

    Se creo el metodo para listar los mensajes enviados por los usuarios

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -32,9 +32,35 @@ function saveMessage(req, res) {
     });
 }
 
+// Metodo para listar los mensajes recibidos
+function getReceivedMessages(req, res) {
+    var userId = req.user.sub;
+
+    var page = 1;
+    if(req.params.page) page = req.params.page;
+
+    var itemsPerPage = 4;
+
+    // Al utilizar el populate, se le puede decir solamente cuales campos se quiere devolver: 'name surname _id nick image'
+    Message.find({receiver: userId}).populate('emitter', 'name surname _id nick image').paginate(page, itemsPerPage, (err, messages, total) => {
+        if (err) return res.status(500).send({message: 'Error en la petición'});
+        if (!messages) return res.status(404).send({message: 'No hay mensajes'});
+
+        // messages.emitter.password = undefined;
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            messages
+        });
+    });
+}
+
 module.exports = {
     // Metodo de prueba
     probando,
     // Metodo para enviar mensaje entre los usuarios
-    saveMessage
+    saveMessage,
+    // Lista los mensajes recibidos
+    getReceivedMessages
 }
--- api/routes/message.js
+++ api/routes/message.js
@@ -7,5 +7,6 @@ var md_auth = require('../middlewares/authenticated');
 
 api.get('/probando-md', md_auth.ensureAuth, messageController.probando);
 api.post('/message', md_auth.ensureAuth, messageController.saveMessage);
+api.get('/my-messages/:page?', md_auth.ensureAuth, messageController.getReceivedMessages);
 
 module.exports = api;
