92599b3f274686b87462a24c0aec424f4d1caa39
MIKE <MiguelMartinezDW@outlook.com>
2019-02-01 21:58:11 -0600

    Resibo los datos del formulario de login

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers:[UserService]
 })
 export class LoginComponent implements OnInit {
   public title:String;
+  public user:User;
 
-  constructor() {
+  constructor(private _route:ActivatedRoute, private _router: Router, private _userService:UserService) {
     this.title = 'Identificate';
+    this.user = new User(
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "ROLE_USER",
+        ""
+    );
   }
 
   ngOnInit() {
-    console.log('Componente de login cargando...')
+    console.log('Componente de login cargando...');
+  }
+
+  onSubmit() {
+    alert(this.user.email);
+    alert(this.user.password);
+    console.log(this.user);
   }
 
 }
